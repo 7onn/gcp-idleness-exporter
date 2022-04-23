c454d2cd5f12439913791e8d8e4b2b00cb65d202
Tom M G <devbytom@gmail.com>
2022-04-23 20:27:09 -0300

    Fix ci error

--- collector/common_test.go
+++ collector/common_test.go
@@ -1,7 +1,6 @@
 package collector
 
 import (
-	"context"
 	"os"
 	"testing"
 
@@ -30,25 +29,6 @@ func TestGetGCPZoneFromURL(t *testing.T) {
 		r := GetGCPZoneFromURL(log.NewJSONLogger(os.Stdout), tc.input)
 		if r != tc.expected {
 			t.Errorf("%s want %s got %s instead", tc.desc, tc.expected, r)
-		}
-	}
-}
-
-func TestNewGCPClient(t *testing.T) {
-	cases := []struct {
-		desc  string
-		input string
-	}{
-		{
-			"Should return a new scoped GCP Client",
-			"https://www.googleapis.com/auth/cloud-platform",
-		},
-	}
-
-	for _, tc := range cases {
-		_, err := NewGCPClient(context.Background(), tc.input)
-		if err != nil {
-			t.Errorf("%s got %+v", tc.desc, err)
 		}
 	}
 }
